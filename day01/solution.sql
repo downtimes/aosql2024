389eb2984afe4be26293e47d1f21723669dcd95d
Michael Auracher <michael.auracher@gmail.com>
2024-12-02 19:32:31 +0100

    Day1 done.

--- day01/solution.sql
+++ day01/solution.sql
@@ -4,7 +4,6 @@ SELECT
     name, 
     w.wishes ->> 'first_choice' as primary_wish, 
     w.wishes ->> 'second_choice' as backup_wish, 
-    -- Unclear if the next 2 fields are needed or not??
     w.wishes ->> '$.colors[0]' as favorite_color,
     json_array_length(w.wishes, '$.colors') as color_count,
     CASE 
@@ -18,8 +17,6 @@ SELECT
         ELSE 'General Workshop'
     END AS workshop_assignment
 FROM children c
--- Unclear if each child should only be allowed one wish_list, but which one??
--- In my input I have 2 children named Abbey with 2 whish_lists each.
 INNER JOIN wish_lists w ON c.child_id == w.child_id
 INNER JOIN toy_catalogue t ON w.wishes ->> 'first_choice' == t.toy_name
 ORDER BY name ASC
